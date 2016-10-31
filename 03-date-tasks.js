7f67cbf9537154096feca8883c48f5ea11a53b11
agent-student <leshka_007@tut.by>
2016-10-31 12:31:51 +0300

    Update 03-date-tasks.js

--- 03-date-tasks.js
+++ 03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+    return new Date(value);
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+  return new Date(value);
 }
 
 
@@ -56,7 +56,9 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+     return ((date.getFullYear()% 4 == 0) &&
+             (date.getFullYear() % 100 != 0)) ||
+             (date.getFullYear() % 400 == 0);
 }
 
 
@@ -76,7 +78,21 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+    var start=new Date(startDate),
+    end=new Date(endDate),
+    delta=new Date(end.getTime()-start.getTime());
+    return ((parseFloat(delta.getUTCHours()) % 10 == 0 
+                && parseFloat(delta.getUTCHours()) != 0 ? "" : "0") +
+            delta.getUTCHours()  + ":" +
+            (parseFloat(delta.getUTCMinutes()) % 10 == 0 &&
+                 parseFloat(delta.getUTCMinutes()) != 0 ? "" : "0") +
+            delta.getUTCMinutes() + ":" +
+            (parseFloat(delta.getUTCSeconds()) % 10 == 0  &&
+                 parseFloat(delta.getUTCSeconds()) != 0 ? "" : "0")  +
+            delta.getUTCSeconds() + "." +
+            (parseFloat(delta.getUTCMilliseconds()) % 1000 == 0 &&
+                 parseFloat(delta.getUTCMilliseconds()) == 0 ? "00" : "") +
+             delta.getUTCMilliseconds());
 }
 
 
@@ -94,7 +110,10 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+    var min=new Date(date).getUTCMinutes(),
+    h=new Date(date).getUTCHours()-12,
+    angle=Math.abs((h*30+min*0.5)-(min*6));
+    return Math.min(angle,360-angle)*Math.PI/180;
 }
 
 
